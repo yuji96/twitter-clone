50e1e4b5f1111952dfe76fc0bf5d7b96ff7e8d3c
Yuji Yamamoto <y.yama683@gmail.com>
2020-08-19 15:26:52 +0900

    ツイート機能を実装

--- tweets/urls.py
+++ tweets/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.TimelineView.as_view(), name='timeline'),
+    path('tweet/new/', views.tweet_new, name='tweet_new'),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,8 @@
+from django.shortcuts import redirect, render
+from django.utils import timezone
 from django.views import generic
 
+from .forms import TweetForm
 from .models import Tweet
 
 
@@ -10,3 +13,17 @@ class TimelineView(generic.ListView):
     def get_queryset(self):
         """Return the last five published questions."""
         return Tweet.objects.order_by('-created_date')[:5]
+
+
+def tweet_new(request):
+    if request.method == "POST":
+        form = TweetForm(request.POST)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.author = request.user
+            tweet.published_date = timezone.now()
+            tweet.save()
+            return redirect('timeline')
+    else:
+        form = TweetForm()
+    return render(request, 'tweets/tweet_new.html', {'form': form})
