cc6c7ba71e3e4ad8124f58aaac6934446e434db1
Yuji Yamamoto <y.yama683@gmail.com>
2020-08-24 13:42:40 +0900

    form_validを削除

--- accounts/views.py
+++ accounts/views.py
@@ -6,8 +6,3 @@ class SignupView(generic.CreateView):
     template_name = 'accounts/signup.html'
     form_class = UserCreationForm
     success_url = 'timeline'
-
-    def form_valid(self, form):
-        user = form.save()
-        user.save()
-        return super().form_valid(form)
